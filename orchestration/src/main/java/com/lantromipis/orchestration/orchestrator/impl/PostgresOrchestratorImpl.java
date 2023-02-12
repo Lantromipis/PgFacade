ac3c12c4801b3eeb757974750c2b1d5a4ddb65d6
lantromipis <mrafomin@gmail.com>
2023-02-12 19:11:37 +0300

    Persist info about instances when creating them

--- orchestration/src/main/java/com/lantromipis/orchestration/orchestrator/impl/PostgresOrchestratorImpl.java
+++ orchestration/src/main/java/com/lantromipis/orchestration/orchestrator/impl/PostgresOrchestratorImpl.java
@@ -3,6 +3,7 @@ package com.lantromipis.orchestration.orchestrator.impl;
 import com.lantromipis.configuration.event.MasterReadyEvent;
 import com.lantromipis.configuration.properties.predefined.OrchestrationProperties;
 import com.lantromipis.configuration.properties.runtime.ClusterRuntimeProperties;
+import com.lantromipis.configuration.properties.stored.api.PersistedProperties;
 import com.lantromipis.orchestration.adapter.api.OrchestrationAdapter;
 import com.lantromipis.orchestration.exception.InstanceCreationException;
 import com.lantromipis.orchestration.model.InstanceHealth;
@@ -12,12 +13,17 @@ import com.lantromipis.orchestration.model.PostgresInstanceInfo;
 import com.lantromipis.orchestration.orchestrator.api.PostgresConfigurator;
 import com.lantromipis.orchestration.orchestrator.api.PostgresOrchestrator;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ApplicationScoped
@@ -37,10 +43,19 @@ public class PostgresOrchestratorImpl implements PostgresOrchestrator {
     @Inject
     PostgresConfigurator postgresConfigurator;
 
+    @Inject
+    ManagedExecutor managedExecutor;
+
+    @Override
     public void initialize() {
         orchestrationAdapter.initialize();
 
-        PostgresInstanceInfo masterInstanceInfo = orchestrationAdapter.getAvailablePostgresInstancesInfos().stream().filter(PostgresInstanceInfo::isMaster).findFirst().orElse(null);
+        //master section
+        PostgresInstanceInfo masterInstanceInfo = orchestrationAdapter.getAvailablePostgresInstancesInfos()
+                .stream()
+                .filter(info -> Boolean.TRUE.equals(info.getMaster()))
+                .findFirst()
+                .orElse(null);
 
         boolean isNewDBSetup = false;
 
@@ -75,18 +90,64 @@ public class PostgresOrchestratorImpl implements PostgresOrchestrator {
 
         masterReadyEvent.fire(new MasterReadyEvent());
 
-        log.info("Creating stand-by");
 
-        UUID instanceId = orchestrationAdapter.createNewPostgresInstance(PostgresInstanceCreationRequest.builder().master(false).postgresqlSettings(Map.of("shared_buffers", "256MB")) //TODO constant for test
-                .build());
-        orchestrationAdapter.startPostgresInstance(instanceId);
+        //standby section
+        List<PostgresInstanceInfo> standbyInfos = orchestrationAdapter.getAvailablePostgresInstancesInfos()
+                .stream()
+                .filter(info -> Boolean.FALSE.equals(info.getMaster()))
+                .toList();
 
-        log.info("Started stand-by");
+        log.info("Found {} standby instances while it is required to have {}", standbyInfos.size(), orchestrationProperties.common().standByCount());
+
+        long countDiff = orchestrationProperties.common().standByCount() - standbyInfos.size();
+
+        if (countDiff > 0) {
+            log.info("Found less standby instances than required. Will create and start {} instances", countDiff);
+
+            List<CompletableFuture<Void>> completableFutureList = new ArrayList<>();
+            for (int i = 0; i < Math.abs(countDiff); i++) {
+                completableFutureList.add(
+                        managedExecutor.runAsync(() -> {
+                            createStartAndWaitForNewInstanceToBeReady(false);
+                            log.info("Standby is up and running!");
+                        })
+                );
+            }
+
+            CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[0])).join();
+        } else {
+            log.info("Found all required standby instances. Will start instances if needed.");
+
+            for (int i = 0; i < orchestrationProperties.common().standByCount(); i++) {
+                PostgresInstanceInfo standbyInfo = standbyInfos.get(i);
+                if (InstanceStatus.ACTIVE.equals(standbyInfo.getStatus())) {
+                    log.info("Standby already active. No actions needed.");
+                } else {
+                    log.info("Starting standby...");
+                    try {
+                        orchestrationAdapter.startPostgresInstance(standbyInfo.getInstanceId());
+                        log.info("Started standby. Will wait until it is healthy...");
+                        waitUntilPostgresInstanceHealthy(standbyInfo.getInstanceId());
+                        log.info("Standby is up ad running!");
+                    } catch (Exception e) {
+                        log.error("Failed to start standby", e);
+                        //TODO do something? or will that do scheduler?
+                    }
+                }
+            }
+        }
     }
 
     private PostgresInstanceInfo createStartAndWaitForNewInstanceToBeReady(boolean master) {
-        UUID instanceId = orchestrationAdapter.createNewPostgresInstance(PostgresInstanceCreationRequest.builder().master(master).postgresqlSettings(Map.of("shared_buffers", "256MB")) //TODO constant for test
-                .build());
+        UUID instanceId = orchestrationAdapter.createNewPostgresInstance(
+                PostgresInstanceCreationRequest
+                        .builder()
+                        .master(master)
+                        .postgresqlSettings(
+                                Map.of("shared_buffers", "256MB") //TODO constant for test
+                        )
+                        .build()
+        );
 
         if (instanceId == null) {
             throw new InstanceCreationException("Can not create new Postgres instance.");
@@ -98,7 +159,7 @@ public class PostgresOrchestratorImpl implements PostgresOrchestrator {
             throw new InstanceCreationException("Can not start new Postgres instance");
         }
 
-        log.info("Created new instance. Will wait until it is healthy...");
+        log.info("Started new instance. Will wait until it is healthy...");
 
         return waitUntilPostgresInstanceHealthy(instanceId);
     }
